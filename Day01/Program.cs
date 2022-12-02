53360eec09093eb0cbbc572f86853f6f95901881
Michael Reisz <michael_reisz@yahoo.com>
2022-12-02 14:26:37 -0500

    Make sure text inputs are copied to build directory

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Read in the Challenge 1 input
-//string[] inputData = System.IO.File.ReadAllLines(@"..\..\..\Challenge1Input_Test.txt");
-string[] inputData = System.IO.File.ReadAllLines(@"..\..\..\Challenge1Input.txt");
+//string[] inputData = System.IO.File.ReadAllLines(@".\Challenge1Input_Test.txt");
+string[] inputData = System.IO.File.ReadAllLines(@".\Challenge1Input.txt");
 
 // This will store the calorie totals for each elf
 List<int> calorieTotals = new();
