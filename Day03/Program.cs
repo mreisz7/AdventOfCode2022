e58776ead3c415ef0e43e8961cb8744a4eb562bd
Michael Reisz <michael_reisz@yahoo.com>
2022-12-03 09:31:19 -0500

    Add Day 3 Challenge 1 solution

---
+++ Day03/Program.cs
@@ -0,0 +1,100 @@
+﻿// Read in the Challenge input
+using System.Reflection.PortableExecutable;
+
+//string[] inputData = System.IO.File.ReadAllLines(@".\ChallengeInput_Test.txt");
+string[] inputData = System.IO.File.ReadAllLines(@".\ChallengeInput.txt");
+
+List<char> mismatchedCharacters = new();
+
+Dictionary<char, int> characterValue = new Dictionary<char, int>()
+{
+    { 'a', 1 },
+    { 'b', 2 },
+    { 'c', 3 },
+    { 'd', 4 },
+    { 'e', 5 },
+    { 'f', 6 },
+    { 'g', 7 },
+    { 'h', 8 },
+    { 'i', 9 },
+    { 'j', 10 },
+    { 'k', 11 },
+    { 'l', 12 },
+    { 'm', 13 },
+    { 'n', 14 },
+    { 'o', 15 },
+    { 'p', 16 },
+    { 'q', 17 },
+    { 'r', 18 },
+    { 's', 19 },
+    { 't', 20 },
+    { 'u', 21 },
+    { 'v', 22 },
+    { 'w', 23 },
+    { 'x', 24 },
+    { 'y', 25 },
+    { 'z', 26 },
+    { 'A', 27 },
+    { 'B', 28 },
+    { 'C', 29 },
+    { 'D', 30 },
+    { 'E', 31 },
+    { 'F', 32 },
+    { 'G', 33 },
+    { 'H', 34 },
+    { 'I', 35 },
+    { 'J', 36 },
+    { 'K', 37 },
+    { 'L', 38 },
+    { 'M', 39 },
+    { 'N', 40 },
+    { 'O', 41 },
+    { 'P', 42 },
+    { 'Q', 43 },
+    { 'R', 44 },
+    { 'S', 45 },
+    { 'T', 46 },
+    { 'U', 47 },
+    { 'V', 48 },
+    { 'W', 49 },
+    { 'X', 50 },
+    { 'Y', 51 },
+    { 'Z', 52 },
+};
+
+foreach (string line in inputData)
+{
+    if (line.Length % 2 != 0)
+        throw new Exception("Unexpected line length");
+
+    string compartment1Values = line.Substring(0, line.Length / 2);
+    string compartment2Values = line.Substring(line.Length / 2, line.Length / 2);
+
+    Console.WriteLine(compartment1Values);
+    Console.WriteLine(compartment2Values);
+
+    HashSet<char> unduplicatedDuplicates = new();
+
+    foreach (char item in compartment1Values)
+    {
+        if (compartment2Values.Contains(item))
+        {
+            unduplicatedDuplicates.Add(item);
+        }
+    }
+
+    mismatchedCharacters.AddRange(unduplicatedDuplicates);
+}
+
+Console.WriteLine("Challenge 1 Answer: " + CalculateValues(mismatchedCharacters));
+
+int CalculateValues(List<char> mismatchedCharacters)
+{
+    int totalValue = 0;
+    foreach (char item in mismatchedCharacters)
+    {
+        totalValue += characterValue[item];
+    }
+
+    return totalValue;
+}
