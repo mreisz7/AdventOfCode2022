33cfe87de64f54c809f578e1a2cea38ae78cd81f
Michael Reisz <michael_reisz@yahoo.com>
2022-12-06 08:59:19 -0500

    Day 6 Challenge 1 solution

---
+++ Day06/Program.cs
@@ -0,0 +1,13 @@
+﻿// Read in the Challenge input
+//string inputData = File.ReadAllText(@".\ChallengeInput_Test.txt").Trim();
+string inputData = File.ReadAllText(@".\ChallengeInput.txt").Trim();
+
+for (int i = 4; i < inputData.Length; i++)
+{
+    HashSet<char> charGroup = new(inputData.Substring(i - 4, 4));
+    if (charGroup.Count == 4)
+    {
+        Console.WriteLine("Challenge 1 Answer: " + i);
+        break;
+    } 
+}
