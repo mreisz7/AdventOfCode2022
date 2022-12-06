1036c061e56f35d5c45aa621e145b4557170c05d
Michael Reisz <michael_reisz@yahoo.com>
2022-12-06 09:05:02 -0500

    Add Day 6 Challenge 2 solution

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -2,12 +2,25 @@
 //string inputData = File.ReadAllText(@".\ChallengeInput_Test.txt").Trim();
 string inputData = File.ReadAllText(@".\ChallengeInput.txt").Trim();
 
+int startOfPacketMarkerLocation = 0;
+int startOfMessageMarkerLocation = 0;
+
 for (int i = 4; i < inputData.Length; i++)
 {
-    HashSet<char> charGroup = new(inputData.Substring(i - 4, 4));
-    if (charGroup.Count == 4)
+    HashSet<char> packetGroup = new(inputData.Substring(i - 4, 4));
+    if (startOfPacketMarkerLocation == 0 && packetGroup.Count == 4)
     {
-        Console.WriteLine("Challenge 1 Answer: " + i);
-        break;
-    } 
+        startOfPacketMarkerLocation = i;
+        Console.WriteLine("Challenge 1 Answer: " + startOfPacketMarkerLocation);
+    }
+
+    if (i >= 14)
+    {
+        HashSet<char> messageGroup = new(inputData.Substring(i - 14, 14));
+        if (startOfMessageMarkerLocation == 0 && messageGroup.Count == 14)
+        {
+            startOfMessageMarkerLocation = i;
+            Console.WriteLine("Challenge 2 Answer: " + startOfMessageMarkerLocation);
+        }
+    }
 }
