64e0119b59730a5142be0cca6a47156c7e3d8a06
Michael Reisz <michael_reisz@yahoo.com>
2022-12-09 16:05:14 -0500

    Add Day 9 Challenge 2 solution

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Read in the Challenge input
-//string[] inputData = File.ReadAllLines(@".\ChallengeInput_Test.txt");
+
+//string[] inputData = File.ReadAllLines(@".\ChallengeInput_Test1.txt");
+//string[] inputData = File.ReadAllLines(@".\ChallengeInput_Test2.txt");
 string[] inputData = File.ReadAllLines(@".\ChallengeInput.txt");
 
 Rope rope = new();
@@ -10,24 +12,31 @@ foreach (string line in inputData)
     rope.MoveHead(instruction.direction, instruction.steps);
 }
 
-HashSet<string> distinctTailSteps = new(rope.TailSteps);
+HashSet<string> distinctTailSteps = new(rope.TailSteps?[1]);
 
 Console.WriteLine($"Challenge 1 Answer: {distinctTailSteps.Count}");
+
+HashSet<string> distinctKnot9Steps = new(rope.TailSteps?[9]);
+
+Console.WriteLine($"Challenge 2 Answer: {distinctKnot9Steps.Count}");
 
 class Rope
 {
     public Coordinates Head { get; set; } = new Coordinates(0, 0);
     public Coordinates Tail { get; set; } = new Coordinates(0, 0);
 
-    public List<string> HeadSteps { get; set; } = new();
-    public List<string> TailSteps { get; set; } = new();
+    public List<string>? HeadSteps { get; set; } = null;
+    public Dictionary<int, List<string>>? TailSteps { get; set; } = null;
+
+    public int Depth { get; init; }
+    public Rope RootKnot { get; init; }
+    public Rope? ParentKnot { get; init; }
+    public Rope? ChildKnot { get; init; } = null;
 
     public void MoveHead(char direction, int steps)
     {
-        Coordinates tempHead = new();
         for (int i = 0; i < steps; i++)
         {
-            tempHead.SetCoordinates(Head);
             switch (direction)
             {
                 case 'U':
@@ -45,12 +54,35 @@ class Rope
                 default:
                     throw new Exception($"Provided direction is not valid ({direction})");
             }
-            HeadSteps.Add(Head.Coords);
-            if (!TailWithinPlankOfHead)
+            if (Depth == 1)
             {
-                Tail.SetCoordinates(tempHead);
-                TailSteps.Add(Tail.Coords);
+                //Console.WriteLine($"Head moved {direction} 1 step");
+                RootKnot.HeadSteps?.Add(Head.Coords);
             }
+            MoveTail();
+        }
+    }
+
+    public void MoveTail()
+    {
+        if (!TailWithinPlankOfHead)
+        {
+            if (Head.X > Tail.X)
+                Tail.X += 1;
+            if (Head.X < Tail.X)
+                Tail.X -= 1;
+            if (Head.Y > Tail.Y)
+                Tail.Y += 1;
+            if (Head.Y < Tail.Y)
+                Tail.Y -= 1;
+
+            // Do a sanity check to make sure that this has put things right before moving on
+            if (!TailWithinPlankOfHead) 
+                throw new Exception("Something went wrong!");
+
+            RootKnot.TailSteps?[Depth].Add(Tail.Coords);
+            ChildKnot?.Head.SetCoordinates(Tail);
+            ChildKnot?.MoveTail();
         }
     }
 
@@ -58,8 +90,34 @@ class Rope
 
     public Rope()
     {
+        HeadSteps = new();
+        TailSteps = new();
+
         HeadSteps.Add(Head.Coords);
-        TailSteps.Add(Tail.Coords);
+        for (int i = 1; i <= 9; i++)
+        {
+            TailSteps.Add(i, new());
+        }
+        TailSteps[1].Add(Tail.Coords);
+
+        RootKnot = this;
+        ParentKnot = null;
+        Depth = 1;
+        ChildKnot = new(this);
+    }
+
+    public Rope(Rope parent)
+    {
+        Head = new(parent.Tail);
+        RootKnot = parent.RootKnot;
+        ParentKnot = parent;
+        Depth = parent.Depth + 1;
+
+        if (Depth < 9)
+        {
+            ChildKnot = new(this);
+        }
+        RootKnot.TailSteps?[Depth].Add(Tail.Coords);
     }
 }
 
