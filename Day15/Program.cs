9858e6b2b22884b929a9ccbf7d67fb8803b2520f
Michael Reisz <michael_reisz@yahoo.com>
2022-12-17 22:50:44 -0500

    Do some day 15 cleanup

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -1,111 +1,124 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections;
+using System.Text.RegularExpressions;
 
 // Read in the Challenge input
 
-string[] inputData = File.ReadAllLines(@".\ChallengeInput_Test.txt");
-int yInQuestion = 10; // <-- Test value
-int minXY = 0;        // <-- Test value
-int maxXY = 20;       // <-- Test value
+//string[] inputData = File.ReadAllLines(@".\ChallengeInput_Test.txt");
+//long yInQuestion = 10; // <-- Test value
+//long minXY = 0;        // <-- Test value
+//long maxXY = 20;       // <-- Test value
 
-//string[] inputData = File.ReadAllLines(@".\ChallengeInput.txt");
-//int yInQuestion = 2_000_000;
-//int minXY = 0;
-//int maxXY = 4000000;
+string[] inputData = File.ReadAllLines(@".\ChallengeInput.txt");
+long yInQuestion = 2_000_000;
+long minXY = 0;
+long maxXY = 4_000_000;
 
 List<SensorBeaconPair> sensors = new();
-HashSet<(int X, int Y, char Type)> cells = new();
 
 foreach (string input in inputData)
 {
     SensorBeaconPair sensor = new SensorBeaconPair(input);
     sensors.Add(sensor);
-    cells.UnionWith(sensor.GetCellsFromY(yInQuestion));
 }
 
-Console.WriteLine($"Challenge 1 Answer: {SolveChallenge1(cells, yInQuestion)}");
+Console.WriteLine($"Challenge 1 Answer: {SolveChallenge1(yInQuestion)}");
 
 Console.WriteLine($"Challenge 2 Answer: {SolveChallenge2(minXY, maxXY)}");
 
-int SolveChallenge1(HashSet<(int X, int Y, char Type)> sensorCells, int y)
+long SolveChallenge1(long y)
 {
-    // Get hashset of SensorBeaconPairs where y matches the provided value
-    HashSet<(int X, int Y)> sensorRangeCells = sensorCells.Where(c => c.Y == y).Select(c => (c.X, c.Y)).ToHashSet();
-
-    // Now subtract the ones that are beacons or sensors
-    int sensorsAndBeacons = sensorCells.Where(c => (c.Type == 'S' || c.Type == 'B') && c.Y == y).Count();
-
-    return sensorRangeCells.Count - sensorsAndBeacons;
-}
-
-int SolveChallenge2(int minXY, int maxXY)
-{
-    int result = -1;
-
-    for (int x = minXY; x <= maxXY; x++)
+    List<(long xStart, long xEnd)> yRanges = new();
+    HashSet<(long x, long y)> yCells = new();
+    
+    // Get the ranges from the y row
+    foreach (SensorBeaconPair sensorPair in sensors)
     {
-        for (int y = minXY; y <= maxXY; y++)
+        (long xStart, long xEnd)? sensorRange = sensorPair.GetCellRangeByY(y);
+        if (sensorRange is not null)
         {
-            foreach (SensorBeaconPair sensor in sensors)
+            (long xStart, long xEnd) range = ((long xStart, long xEnd))sensorRange;
+            for (long x = range.xStart; x <= range.xEnd; x++)
             {
-                if ()
+                yCells.Add((x, y));
             }
         }
     }
 
-    return result;
+    // Get the count of beacons on the row in question
+    HashSet<(long x, long y)> beaconsOnY = sensors.Where(x => x.Beacon.Y == y).Select(x => (x.Beacon.X, x.Beacon.Y)).ToHashSet();
+
+    return yCells.Count - beaconsOnY.Count;
+}
+
+long SolveChallenge2(long minXY, long maxXY)
+{
+    for (long y = minXY; y <= maxXY; y++)
+    {
+        for (long x = minXY; x <= maxXY; x++)
+        {
+            List<SensorBeaconPair> sensorsInRange = sensors.Where(s => s.IsWithinCoverage((x, y))).ToList();
+            if (sensorsInRange.Count > 0)
+            {
+                x = sensorsInRange.Select(s => s.GetCellRangeByY(y)).Select(s => s.Value.xEnd).Max();
+            } else
+            {
+                return x * 4_000_000 + y;
+            }
+
+        }
+    }
+
+    throw new Exception("Nothing found");
 }
 
 partial class SensorBeaconPair
 {
-    public (int X, int Y) Sensor { get; init; }
+    public (long X, long Y) Sensor { get; init; }
 
-    public (int X, int Y) Beacon { get; init; }
+    public (long X, long Y) Beacon { get; init; }
 
-    public int ManhattanDistance { get; init; }
+    public long ManhattanDistance { get; init; }
 
-    public static int GetManhattanDistance((int X, int Y) sensor, (int X, int Y) beacon)
+    public static long GetManhattanDistance((long X, long Y) sensor, (long X, long Y) beacon)
     {
-        int maxX = Math.Max(sensor.X, beacon.X);
-        int minX = Math.Min(sensor.X, beacon.X);
-        int maxY = Math.Max(sensor.Y, beacon.Y);
-        int minY = Math.Min(sensor.Y, beacon.Y);
+        long maxX = Math.Max(sensor.X, beacon.X);
+        long minX = Math.Min(sensor.X, beacon.X);
+        long maxY = Math.Max(sensor.Y, beacon.Y);
+        long minY = Math.Min(sensor.Y, beacon.Y);
 
         return (maxX - minX) + (maxY - minY);
     }
 
-    public HashSet<(int X, int Y, char Type)> GetCellsFromY(int y)
+    public bool IsWithinCoverage((long x, long y) coordinate)
     {
-        HashSet<(int X, int Y, char Type)> cellSet = new();
-
-        for (int yOffset = -ManhattanDistance; yOffset <= ManhattanDistance; yOffset++)
+        if (GetManhattanDistance(Sensor, (coordinate.x, coordinate.y)) <= ManhattanDistance)
         {
-            if (Sensor.Y + yOffset == y)
-            {
-                for (int xOffset = -(ManhattanDistance - Math.Abs(yOffset)); xOffset <= ManhattanDistance - Math.Abs(yOffset); xOffset++)
-                {
-                    (int X, int Y) cellCoordinate = (Sensor.X + xOffset, Sensor.Y + yOffset);
-                    char type = '#';
-                    if (cellCoordinate == Sensor)
-                        type = 'S';
-                    if (cellCoordinate == Beacon)
-                        type = 'B';
-                    cellSet.Add((cellCoordinate.X, cellCoordinate.Y, type));
-                }
-            }
+            return true;
         }
+        return false;
+    }
 
-        return cellSet;
-    } 
+    public (long xStart, long xEnd)? GetCellRangeByY(long requestedY)
+    {
+        long yOffset = requestedY - Sensor.Y;
+
+        if (yOffset < -ManhattanDistance || yOffset > ManhattanDistance)
+            return null;
+
+        long xStart = Sensor.X - (ManhattanDistance - Math.Abs(yOffset));
+        long xEnd = Sensor.X + (ManhattanDistance - Math.Abs(yOffset));
+        return (xStart, xEnd);
+    }
 
     public SensorBeaconPair(string sensorData)
     {
         // Example input: Sensor at x=2, y=18: closest beacon is at x=-2, y=15
         Regex sensorInput = MyRegex();
         Match match = sensorInput.Match(sensorData);
-        int sensorX = int.Parse(match.Groups["sensorX"].Value);
-        int sensorY = int.Parse(match.Groups["sensorY"].Value);
-        int beaconX = int.Parse(match.Groups["beaconX"].Value);
-        int beaconY = int.Parse(match.Groups["beaconY"].Value);
+        long sensorX = long.Parse(match.Groups["sensorX"].Value);
+        long sensorY = long.Parse(match.Groups["sensorY"].Value);
+        long beaconX = long.Parse(match.Groups["beaconX"].Value);
+        long beaconY = long.Parse(match.Groups["beaconY"].Value);
 
         Sensor = (sensorX, sensorY);
         Beacon = (beaconX, beaconY);
