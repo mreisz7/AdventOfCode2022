df23f0ad9d8868309f2003a86ec1e759f05128ca
Michael Reisz <michael_reisz@yahoo.com>
2022-12-26 17:14:40 -0500

    Add Day 21 Challenge 2 solution

--- Day21/Program.cs
+++ Day21/Program.cs
@@ -17,8 +17,19 @@ foreach (string line in inputData)
     }
 }
 
-long FindValue(string input)
+long rootAnswer = FindValue("root");
+
+//Console.WriteLine($"Challenge 1 Answer: {rootAnswer}");
+
+Console.WriteLine($"Challenge 2 Answer: {FindRootEquality("root")}");
+
+long FindValue(string input, long? humanInput = null)
 {
+    if (humanInput != null && input == "humn")
+    {
+        return (long)humanInput;
+    }
+
     if (monkeys.TryGetValue(input, out (string input, long? number) value) && value.number != null)
     {
         return (long)monkeys[input].number;
@@ -33,16 +44,16 @@ long FindValue(string input)
     switch (mathOperator)
     {
         case "+":
-            result = FindValue(input1) + FindValue(input2);
+            result = FindValue(input1, humanInput) + FindValue(input2, humanInput);
             break;
         case "-":
-            result = FindValue(input1) - FindValue(input2);
+            result = FindValue(input1, humanInput) - FindValue(input2, humanInput);
             break;
         case "*":
-            result = FindValue(input1) * FindValue(input2);
+            result = FindValue(input1, humanInput) * FindValue(input2, humanInput);
             break;
         case "/":
-            result = FindValue(input1) / FindValue(input2);
+            result = FindValue(input1, humanInput) / FindValue(input2, humanInput);
             break;
         default:
             throw new NotImplementedException("Operator not supported");
@@ -51,6 +62,100 @@ long FindValue(string input)
     return result;
 }
 
-long rootAnswer = FindValue("root");
+long FindRootEquality(string monkey)
+{
+    string[] inputs = monkeys[monkey].input.Split(' ');
+    string input1 = inputs[0];
+    string mathOperator = inputs[1];
+    string input2 = inputs[2];
 
-Console.WriteLine($"Challenge 1 Answer: {rootAnswer}");
+    // Find which side has the human input
+    long leftSideValue = FindValue(input1);
+    long rightSideValue = FindValue(input2);
+
+    long leftSideAltered = FindValue(input1, 42);
+    long rightSideAltered = FindValue(input2, 42);
+
+    long mustEqual, humanValue;
+
+    if (leftSideValue != leftSideAltered) 
+    {
+        mustEqual = rightSideValue;
+        humanValue = FindHumanValue(input1, mustEqual);
+    }
+    else
+    {
+        mustEqual = leftSideValue;
+        humanValue = FindHumanValue(input2, mustEqual);
+    }
+
+    return humanValue;
+}
+
+long FindHumanValue(string monkey, long valueMustEqual)
+{
+    if (monkey == "humn")
+    {
+        return valueMustEqual;
+    }
+
+    string[] inputs = monkeys[monkey].input.Split(' ');
+    string input1 = inputs[0];
+    string mathOperator = inputs[1];
+    string input2 = inputs[2];
+
+    // Find which side has the human input
+    long leftSideValue = FindValue(input1);
+    long rightSideValue = FindValue(input2);
+
+    long leftSideAltered = FindValue(input1, 42);
+    long rightSideAltered = FindValue(input2, 42);
+
+    long mustEqual = 0;
+    long humanValue;
+
+    if (leftSideValue != leftSideAltered)
+    {
+        switch (mathOperator)
+        {
+            case "*":
+                mustEqual = valueMustEqual / rightSideValue;
+                break;
+            case "/":
+                mustEqual = rightSideValue * valueMustEqual;
+                break;
+            case "+":
+                mustEqual = valueMustEqual - rightSideValue;
+                break;
+            case "-":
+                mustEqual = rightSideValue + valueMustEqual;
+                break;
+            default:
+                throw new NotImplementedException("Operator not supported");
+        }
+        humanValue = FindHumanValue(input1, mustEqual);
+    }
+    else
+    {
+        switch (mathOperator)
+        {
+            case "*":
+                mustEqual = valueMustEqual / leftSideValue;
+                break;
+            case "/":
+                mustEqual = leftSideValue / valueMustEqual;
+                break;
+            case "+":
+                mustEqual = valueMustEqual - leftSideValue;
+                break;
+            case "-":
+                mustEqual = leftSideValue - valueMustEqual;
+                break;
+            default:
+                throw new NotImplementedException("Operator not supported");
+        }
+        humanValue = FindHumanValue(input2, mustEqual);
+    }
+
+    return humanValue;
+}
